0f9ea695d96b6838ac5d3c98e9ca87e96dbb2970
Stanly0407 <Shelestova_S_V@mail.ru>
2019-12-10 20:36:24 +0300

    init

---
+++ src/by/belhard/j19/lessons/lesson2/primitiveExample.java
@@ -0,0 +1,20 @@
+package by.belhard.j19.lessons.lesson2;
+
+public class primitiveExample {
+
+
+    public static void main(String[] args) {
+     /*short sh = 5;
+     int i = 100;
+     double d = 3.14;
+     System.out.println(sh + i / d);
+     */
+     int i = 0;
+
+     System.out.println(i++);
+
+
+     System.out.println(i--);
+
+    }
+}
