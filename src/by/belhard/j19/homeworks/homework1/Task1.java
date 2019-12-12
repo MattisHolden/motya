d6b44a69a3ccf1e227f9bbe457d1b595252aab09
Mattis <Mattis.holden@mail.ru>
2019-12-12 18:18:14 +0300

    motya

---
+++ src/by/belhard/j19/homeworks/homework1/Task1.java
@@ -0,0 +1,20 @@
+package by.belhard.j19.homeworks.homework1;
+
+public class Task1 {
+
+
+    public static void main(String[] args) {
+
+        byte z = 4;
+
+        System.out.println(z / 2 - 2);
+
+        char a = 10;
+
+        System.out.println(a + 2);
+
+        double s = 4.45;
+        System.out.println(s - a * z);
+
+    }
+}
---
+++ src/by/belhard/j19/homeworks/homework1/Task2.java
@@ -0,0 +1,16 @@
+package by.belhard.j19.homeworks.homework1;
+
+public class Task2 {
+
+
+    public static void main(String[] args) {
+
+        int r = 5;
+        double p = 3.14;
+        System.out.println(2*p*r);
+
+        // площадь квадрата будет равна :
+        int sqr = r*r;
+        System.out.println(p*sqr);
+    }
+}
